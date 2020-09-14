22ca82432647479bf64bea788a558537d13c8a5f
Upendra Samaranayake <humanfrailtz@gmail.com>
2020-09-14 14:25:21 -0700

    Initial commit

---
+++ HW3-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Exercise1
+{
+
+	public static void Main(string[] Strings)
+	{
+		Console.Write("Input a degree in Fahrenheit: ");
+
+		Double input = Convert.ToDouble(Console.ReadLine());
+
+
+		double fahrenheit = input;
+
+		double celsius = ((5 * (fahrenheit - 32.0)) / 9.0);
+		Console.WriteLine(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
+	}
+}
